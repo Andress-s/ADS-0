3c682128b75c83bcc13b97c6cad6ea70dd9b41b3
Andresss <53404779+Andress-s@users.noreply.github.com>
2023-02-19 11:17:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,8 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    if (a%b==0) return b;
-    if (b%a==0) return a;
-    if (a>b) return gcd(a%b,b);
-    return gcd(a,b%a);
+    if (a%b == 0) return b;
+    if (b%a == 0) return a;
+    if (a > b) return gcd(a%b, b);
+    return gcd(a, b%a);
 }
